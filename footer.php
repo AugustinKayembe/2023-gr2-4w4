5eca2a3e1c74be8ef903a27e946ee2957a95de75
Augustin kayembe <Augustin kayembe>
2023-02-06 12:19:50 -0500

    s2c2 lab2

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
-<footer>
+<footer class="site__footer">
+     <section>Colonne 1</section>
+     <section>Colonne 1</section>
+     <section>Colonne 1</section>
     <h3>Le pied de page</h3>
 </footer>
 <?php
