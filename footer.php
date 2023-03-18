7621eea7aa6b181107f6b207baba5f7c982d2a6c
Augustin Kayembe <99110941+AugustinKayembe@users.noreply.github.com>
2023-03-18 01:14:44 -0400

    Modification footer et header

--- footer.php
+++ footer.php
@@ -1,11 +1,16 @@
 <footer class="site__footer">
-     <section>Colonne 1</section>
-     <section>Colonne 1</section>
-     <section>Colonne 1</section>
-    <h3 class="pied2page_titre"></h3>
-</footer>
-<?php
-wp_footer();
-?>
+    <section> <?php the_custom_logo(); ?></section>
+    <!-- <a<sectionhref="https://github.com/AugustinKayembe">Accédez à mon Github</a></section> -->
 
+   <section class="site__footer__link">
+   <a href="https://github.com/AugustinKayembe">Accédez à mon Github</a>
+   </section>
+
+    <section><div class="menurecherche">
+            <input type="checkbox" id="chkMenu">
+            <?php  get_search_form(); ?>
+      </div>  </section>
+</footer>
+<?php wp_footer(); ?>
+</body>
 </html>
