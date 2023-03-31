2ac2572897650b2b67c2157c99b7f44c0c9e827f
Augustin Kayembe <99110941+AugustinKayembe@users.noreply.github.com>
2023-03-31 10:46:55 -0400

    Sélection de la catégorie de l’article

--- search.php
+++ search.php
@@ -11,17 +11,25 @@
     <h3>search.php</h3>
     <h2>Résultats de la recherche</h2>
     <section class="article_solo">
-    <?php if (have_posts()) :
+    <?php  if (have_posts()): 
         while (have_posts()) : the_post();
-            the_title('<h1>', '</h1>');
-            //the_content(); ?>
-            <?php wp_trim_words(get_the_excerpt(), 50, "[...]"); ?>
+        if (in_category('4w4')){
+           ?>
+            <?php  get_template_part("template-parts/search", "4w4");  ?>
             <hr>
-    <?php
-        endwhile;
-    endif;
-    ?>
-</section>
+
+        <?php 
+        }else if (in_category('cours')){
+            ?>
+           <?php get_template_part("template-parts/search", "cours");  ?>
+            <hr>
+        <?php }else{ ?>
+            <?php  get_template_part("template-parts/search", "4w4"); }?>
+
+        <?php endwhile;
+    endif; ?>
+    
+  </section>
 
 </main>
 <?php get_footer(); ?>
