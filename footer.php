a14bdca1fea58328267ca279587f099f1c45d734
Augustin Kayembe <99110941+AugustinKayembe@users.noreply.github.com>
2023-04-28 08:53:51 -0400

    Ajout de widget dans footer

--- footer.php
+++ footer.php
@@ -1,15 +1,22 @@
 <footer class="site__footer">
-    <section> <?php the_custom_logo(); ?></section>
-    <!-- <a<sectionhref="https://github.com/AugustinKayembe">Accédez à mon Github</a></section> -->
+    <section><?php the_custom_logo(); ?></section>
 
-   <section class="site__footer__link">
+<section class="site__footer__link">
    <a href="https://github.com/AugustinKayembe">Accédez à mon Github</a>
-   </section>
+</section>
+
+    <section>
+        <div class="sidebar">
+            <?php dynamic_sidebar( 'footer_1' ); ?>
+        </div>
+    </section>
 
     <section><div class="menurecherche">
             <input type="checkbox" id="chkMenu">
             <?php  get_search_form(); ?>
-      </div>  </section>
+      </div>  
+</section>
+
 </footer>
 <?php wp_footer(); ?>
 </body>
--- front-page.php
+++ front-page.php
@@ -7,6 +7,16 @@
 <?php get_header(); ?>
 <main class="site__main">
     <h3></h3>
+
+    <section class="blocflex">
+
+    <?php wp_nav_menu(array(
+        "menu" => "evenement",
+        "container" => "nav"
+      ));
+    ?>
+
+    </section>
     <section class="blocflex">
     <?php
         if (have_posts()): 
