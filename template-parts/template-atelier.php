5560e023f73e5247675944a42815a915268f39e4
Augustin Kayembe <99110941+AugustinKayembe@users.noreply.github.com>
2023-05-23 12:07:17 -0400

    Ajout du fichier -menu_atelier.scss

--- template-parts/template-atelier.php
+++ template-parts/template-atelier.php
@@ -1,10 +1,23 @@
-<aside class="aside-atelier">
-    <h4>Informations de l'atelier</h4>
-    <ul>
-        <li>Formateur : <?php echo get_field('formateur_'); ?></li>
-        <li>Date de début : <?php echo get_field('date_de_latelier_'); ?></li>
-        <li>Heure de la formation : <?php echo get_field('heure_de_la_formation_'); ?></li>
-        <li>Durée  : <?php echo get_field('duree_'); ?></li>
-        <li>Local : <?php echo get_field('local_'); ?></li>
-    </ul>
-</aside>
+<?php
+/**
+ * Template Name: Atelier
+ */
+get_header();
+?>
+
+<main class="site__main">
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php the_post_thumbnail('thumbnail'); ?>
+            <h1><?php echo get_the_title(); ?></h1>
+            <?php the_content(); ?>
+            <p>Formateur <?php the_field('formateur_'); ?></p>
+            <p>Date de début <?php the_field('date_de_debut_'); ?></p>
+            <p>Heure de début <?php the_field('heure_de_debut_'); ?></p>
+            <p>Durée <?php the_field('duree_'); ?></p>
+            <p>Local <?php the_field('local_'); ?></p>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</main><!-- #main -->
+
+<?php get_footer(); ?>
