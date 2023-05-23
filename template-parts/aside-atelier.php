a03f685ea025b44b89f99298206f41e3e9701817
Augustin Kayembe <99110941+AugustinKayembe@users.noreply.github.com>
2023-05-23 11:44:49 -0400

    Ajout du fichier template-atelier

--- template-parts/aside-atelier.php
+++ template-parts/aside-atelier.php
@@ -1,5 +1,5 @@
 <aside class="site__aside">
-
+<?php get_template_part('template-part-aside-atelier'); ?>
 <h3>Menu Atelier 2023 </h3>
 <?php 
 $ma_categorie = "4w4";
---
+++ template-parts/template-atelier.php
@@ -0,0 +1,10 @@
+<aside class="aside-atelier">
+    <h4>Informations de l'atelier</h4>
+    <ul>
+        <li>Formateur : <?php echo get_field('formateur_'); ?></li>
+        <li>Date de début : <?php echo get_field('date_de_latelier_'); ?></li>
+        <li>Heure de la formation : <?php echo get_field('heure_de_la_formation_'); ?></li>
+        <li>Durée  : <?php echo get_field('duree_'); ?></li>
+        <li>Local : <?php echo get_field('local_'); ?></li>
+    </ul>
+</aside>
