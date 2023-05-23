a6e73b2044f66424b48abf8400a6e00ff9af8492
Augustin Kayembe <99110941+AugustinKayembe@users.noreply.github.com>
2023-05-23 13:12:01 -0400

    Modification du fichier template-atelier

--- template-atelier.php
+++ template-atelier.php
@@ -11,11 +11,11 @@ get_header();
             <?php the_post_thumbnail('thumbnail'); ?>
             <h1><?php echo get_the_title(); ?></h1>
             <?php the_content(); ?>
-            <p>Formateur <?php the_field('formateur_'); ?></p>
-            <p>Date de début <?php the_field('date_de_debut_'); ?></p>
-            <p>Heure de début <?php the_field('heure_de_debut_'); ?></p>
-            <p>Durée <?php the_field('duree_'); ?></p>
-            <p>Local <?php the_field('local_'); ?></p>
+            <p>Formateur : <?php the_field('formateur_'); ?></p>
+            <p>Date de début : <?php the_field('date_de_latelier_'); ?></p>
+            <p>Heure de début : <?php the_field('heure_de_la_formation_'); ?></p>
+            <p>Durée : <?php the_field('duree_'); ?></p>
+            <p>Local : <?php the_field('local_'); ?></p>
         <?php endwhile; ?>
     <?php endif; ?>
 </main><!-- #main -->
